4536f932901bb5d6ef3cccd73de849483ffd9a52
Jess D. T. Tubbs <jthornberry24@gmail.com>
2024-01-12 21:25:02 -0600

    Input Getters/Setters

--- Module1/SeatReservation.java
+++ Module1/SeatReservation.java
@@ -1,6 +1,48 @@
 public class SeatReservation {
-    private String flightDesignator
-    private java.time.LocalDate flightDate
-    private String firstName
-    private String lastName
+    private String flightDesignator;
+    private java.time.LocalDate flightDate;
+    private String firstName;
+    private String lastName;
+
+    public String getflightDesignator() {
+        return null;
+    }
+
+    public void setFlightDesignator(String fd) {
+
+    }
+
+    public java.time.LocalDate getFlightDate() {
+        return null;
+    }
+
+    public void setFlightDate(java.time.LocalDate date) {
+
+    }
+
+    public String getFirstName() {
+        return null;
+    }
+
+    public void setFirstName(String fn) {
+
+    }
+
+    public String getLastName() {
+        return null;
+    }
+
+    public void setLastName(String ln) {
+
+    }
+
+    public String toString() {
+        return "SeatReservation{" +
+                "flightDesignator=" + (flightDesignator != null ? flightDesignator : "null") +
+                ", flightDate=" + (flightDate != null ? flightDate.toString() : "null") +
+                ", firstName=" + (firstName != null ? firstName : "null") +
+                ", lastName=" + (lastName != null ? lastName : "null") +
+                '}';
+    }
+
 }
